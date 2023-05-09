de0beaf1a126f64241abc8d0936c4aa40c00799d
Ali-Fikri <alifikripr@gmail.com>
2023-05-09 04:46:20 +0200

    Add useEffect to use local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 import "./app.css";
-import { NewTodoForm } from './NewTodoForm';
-import { TodoList } from './TodoList';
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
 function App() {
-  const [todo, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+
+    return localValue == null ? [] : JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (title) => {
     setTodos((currentTodos) => [
-          ...currentTodos,
-          { id: crypto.randomUUID(), title, isCompleted: false },
-        ]);
-  }
-
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, isCompleted: false },
+    ]);
+  };
 
   const toggleTodo = (id, isCompleted) => {
     setTodos((currentTodos) => {
@@ -29,9 +37,9 @@ function App() {
 
   return (
     <>
-      <NewTodoForm onSubmit={addTodo}/>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <TodoList todos={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 }
